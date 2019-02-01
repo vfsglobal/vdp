c7264c2f934c740d2c07852c6d9670d556e58b99
maheshshah196 <maheshshah196@gmail.com>
2019-02-01 13:00:32 +0530

    started responsive and also created in depth space_details

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,7 +9,7 @@ module.exports = {
   head: {
     meta: [
       { charset: "utf-8" },
-      //{ name: "viewport", content: "width=device-width, initial-scale=1" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
       { hid: "keywords", name: "keywords", content: "" },
       { hid: "description", name: "description", content: "" }
     ],
