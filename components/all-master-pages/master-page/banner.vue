af65832cb406765ccaef19645e0ae8b6455dad1b
Ankita Singh <Temp_AnkitaS@Vfsglobal.com>
2019-02-04 17:47:29 +0530

    banner responsive done

--- components/all-master-pages/master-page/banner.vue
+++ components/all-master-pages/master-page/banner.vue
@@ -48,7 +48,7 @@ export default {
     (
       height: (
         default: 450px,
-        _onlySdesktop: 400px,
+        _onlySdesktop: 380px,
         _onlymobile: 300px
       ),
       margin-bottom:
@@ -65,6 +65,9 @@ export default {
   align-items: center;
   z-index: 1;
   > .wrapper > .main_content {
+    $shadow_modify_option: (
+      suffix: " " + $shadow_color
+    );
     @include multi_media(padding-bottom, $bottom_overlap_space);
     text-align: center;
     color: #fff;
@@ -72,17 +75,18 @@ export default {
       @include multi_media(
         text-shadow,
         (
-          default: 3px 3px 3px $shadow_color,
-           _onlySdesktop: 2px 2px 2px $shadow_color,
-        _onlymobile: 1px 1px 1px $shadow_color
-        )
+          default: 3px 3px 3px,
+          _onlySdesktop: 2px 2px 2px,
+          _onlymobile: 1px 1px 1px
+        ),
+        $shadow_modify_option
       );
       @extend %H_font_size;
       font-weight: bold;
     }
     > p {
       @extend %L2_font_size;
-      line-height: 1.125em;
+      line-height: 1.6em;
       @extend %padt_normal;
     }
     > a.main_button {
@@ -94,19 +98,23 @@ export default {
       color: #fff;
       @extend %L_font_size;
       @include add_css_from_map(
-    (
-      border-radius: (
-        default: 10px,
-         _onlySdesktop: 8px,
-        _onlymobile: 7px
-        ),
-        box-shadow:(
-          default:5px 5px 5px $shadow_color,
-          _onlySdesktop: 4px 4px 4px $shadow_color,
-        _onlymobile: 3px 3px 3px $shadow_color
+        (
+          border-radius: (
+            default: 10px,
+            _onlySdesktop: 7px,
+            _onlymobile: 5px
+          ),
+          box-shadow:
+            modify_map_value(
+              (
+                default: 5px 5px 5px,
+                _onlySdesktop: 4px 4px 4px,
+                _onlymobile: 3px 3px 3px
+              ),
+              $shadow_modify_option
+            )
         )
-    ));
-     
+      );
       transition: all 0.3s;
       &:hover {
         transform: scale(1.1);
