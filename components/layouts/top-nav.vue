e14c09b966296467c0d3c3789330a48438aece43
maheshshah196 <maheshshah196@gmail.com>
2019-02-04 19:15:36 +0530

    basic changes for responsive like normal value to map with multiple media, etc.

--- components/layouts/top-nav.vue
+++ components/layouts/top-nav.vue
@@ -1,10 +1,10 @@
 <template>
-    <nav>
-        <div class="wrapper">
-            <menu-nav :content="navContent" />
-            
-        </div>
-    </nav>
+  <nav>
+    <div class="wrapper">
+      <menu-nav :content="navContent" />
+      <button class="responsive_toggle">Menu</button>
+    </div>
+  </nav>
 </template>
 
 <script>
@@ -13,14 +13,14 @@ import menuNav from '~/components/utils/menu-nav.vue';
 import { mapState } from 'vuex';
 
 export default {
-    components: {
-        menuNav
-    },
-    computed: {
-      ...mapState('top-nav', {
-          navContent: state => state.list
-      })
-    }
+  components: {
+    menuNav
+  },
+  computed: {
+    ...mapState('top-nav', {
+      navContent: state => state.list
+    })
+  }
 }
 </script>
 
@@ -28,35 +28,55 @@ export default {
 @import "./assets/scss/globals";
 
 nav {
-  $height: 50px;
+  $height: (
+    default: 50px,
+    _onlySdesktop: 45px,
+    _onlymobile: 40px
+  );
 
   background: $purple;
+  %height_with_line_height {
+    @include add_css_from_map(
+      (
+        height: $height,
+        line-height: $height
+      )
+    );
+  }
   ul.menu_nav {
     display: flex;
     justify-content: space-between;
-
+    @include media(Stablet) {
+      display: none;
+    }
     > li {
-      $link_hover_border_height: 3px;
-      $link_hover_border_radius: $link_hover_border_height;
-
+      $link_hover_border_height: (
+        default: 3px,
+        _onlySdesktop: 2px,
+        _onlymobile: 1px
+      );
+      $link_hover_border_radius: modify_map_value(
+        $link_hover_border_height,
+        (
+          repeat: 2
+        )
+      );
       %hover {
-        &:before, &:after {
-          height: $link_hover_border_height;
+        &:before,
+        &:after {
+          @include multi_media(height, $link_hover_border_height);
         }
       }
-      
       > a {
         position: relative;
         display: block;
-        height: $height;
-        line-height: $height;
-        padding: 0px 5px;
+        @extend %height_with_line_height;
+        @extend %padh_extra_mini;
         color: #fff;
         text-transform: uppercase;
-        font-size: 12px;
+        @extend %S_font_size;
         font-weight: bold;
         transition: 0.3s;
-
         &:before,
         &:after {
           content: "";
@@ -69,20 +89,43 @@ nav {
         }
         &:before {
           top: 0%;
-          border-radius: 0 0 $link_hover_border_radius $link_hover_border_radius;
+          @include multi_media(
+            border-radius,
+            $link_hover_border_radius,
+            (
+              prefix: "0 0 "
+            )
+          );
         }
         &:after {
           bottom: 0%;
-          border-radius: $link_hover_border_radius $link_hover_border_radius 0 0;
+          @include multi_media(
+            border-radius,
+            $link_hover_border_radius,
+            (
+              suffix: " 0 0"
+            )
+          );
         }
         &:hover {
           @extend %hover;
         }
       }
-
-      &:first-child > a.nuxt-link-exact-active, &:not(:first-child) > a.nuxt-link-active {
+      &:first-child > a.nuxt-link-exact-active,
+      &:not(:first-child) > a.nuxt-link-active {
         @extend %hover;
       }
+    }
+  }
+  button.responsive_toggle {
+    display: none;
+    width: 100%;
+    color: #fff;
+    @extend %height_with_line_height;
+    outline: none;
+    
+    @include media(Stablet) {
+      display: block;
     }
   }
 }
