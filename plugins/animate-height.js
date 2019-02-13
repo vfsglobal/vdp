947d65e134a88c7815fa9858a95fd647b6a3c5e5
maheshshah196 <maheshshah196@gmail.com>
2019-02-13 16:21:08 +0530

    animate-height plugin solved

--- plugins/animate-height.js
+++ plugins/animate-height.js
@@ -4,19 +4,20 @@ import { animateHeight } from "~/utils";
 function hook(el, binding, vnode) {
   var $el = $(el),
     el_height = $el.height();
-
-  //if (el_height == $el.data("animate-height")) return;
-
-  //$el.data("animate-height", $el.height());
-
-  //options = autocorrectOptions(binding);
+  
+  if (el_height == $el.data("animate-current-height")) return;
+  
+  $el.data("animate-current-height", el_height);
 
   $el.stop().css("height", el_height + "px");
-
+  
   vnode.context.$nextTick(function() {
     animateHeight($el, binding.value);
   });
-  //   console.log($(el).height());
+
+  setTimeout(function() {
+    $el.removeData("animate-current-height");
+  });
 }
 
 Vue.directive("animate-height", {
