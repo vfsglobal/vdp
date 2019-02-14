fdba2c70891975cefc9605ea9985a73aac8a9a55
maheshshah196 <maheshshah196@gmail.com>
2019-02-14 20:04:24 +0530

    removed checking of animate-current-height to provide animation if option changes

--- plugins/animate-height.js
+++ plugins/animate-height.js
@@ -4,19 +4,11 @@ import { animateHeight } from "~/utils";
 function hook(el, binding, vnode) {
   var $el = $(el),
     el_height = $el.height();
-  
-  if (el_height == $el.data("animate-current-height")) return;
-  
-  $el.data("animate-current-height", el_height);
 
   $el.stop().css("height", el_height + "px");
   
   vnode.context.$nextTick(function() {
     animateHeight($el, binding.value);
-  });
-
-  setTimeout(function() {
-    $el.removeData("animate-current-height");
   });
 }
 
